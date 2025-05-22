661eb1bbbbf539dd635fb24287bd1613934136d1
Jack Gavin <jackdgavin@outlook.com>
2025-05-22 16:32:00 -0700

    Initial Commit

---
+++ ClaudeTests/ClaudeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeTests.swift
+//  ClaudeTests
+//
+//  Created by Jack Gavin on 5/22/25.
+//
+
+import Testing
+@testable import Claude
+
+struct ClaudeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
